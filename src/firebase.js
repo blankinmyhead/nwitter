7f713d20aed88311d9194b74f169b9e160afc630
jhyunwoo <jhyunwoo0228@gmail.com>
2022-08-30 22:31:55 +0900

    fix error in firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from "firebase/app";
+const firebaseConfig = {
+    apiKey: "상혁이 걸로 바꾸기",
+    authDomain: "상혁이 걸로 바꾸기",
+    projectId: "상혁이 걸로 바꾸기",
+    storageBucket: "상혁이 걸로 바꾸기",
+    messagingSenderId: "상혁이 걸로 바꾸기",
+    appId: "상혁이 걸로 바꾸기",
+    measurementId: "상혁이 걸로 바꾸기"
+};
+export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-
+import firebase from "firebase/compat/app";
+// import "firebase/compat/auth";
+// import "firebase/compat/firestore";
+// import "firebase/compat/storage";
+console.log(firebase)
 ReactDOM.render(
-  
   <React.StrictMode>
     <App />
   </React.StrictMode>,
